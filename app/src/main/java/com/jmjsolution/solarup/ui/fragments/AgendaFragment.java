0dae6485947766a926ec705ac998ea3b64287788
samuelzaffran <samuel2629@gmail.com>
2018-12-04 22:25:39 +0200

    commit 1.1

--- app/src/main/java/com/jmjsolution/solarup/ui/fragments/AgendaFragment.java
+++ app/src/main/java/com/jmjsolution/solarup/ui/fragments/AgendaFragment.java
@@ -1,20 +1,33 @@
 package com.jmjsolution.solarup.ui.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jmjsolution.solarup.R;
 import com.jmjsolution.solarup.adapters.EventAdapter;
@@ -34,14 +47,19 @@ import butterknife.ButterKnife;
 import static android.app.Activity.RESULT_OK;
 import static com.jmjsolution.solarup.utils.CalendarService.MY_PREFS_NAME;
 
-public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
+public class AgendaFragment extends Fragment implements EventsCalendar.Callback {
 
     private static final int TIME_PICKER = 13;
-    @BindView(R.id.eventsCalendar) EventsCalendar eventsCalendar;
-    @BindView(R.id.eventsRv) RecyclerView mEventsRv;
-    @BindView(R.id.addEventFab) FloatingActionButton mAddEventBtn;
-    @BindView(R.id.configureCountTv) TextView mConfigureCompteTv;
+    @BindView(R.id.eventsCalendar)
+    EventsCalendar eventsCalendar;
+    @BindView(R.id.eventsRv)
+    RecyclerView mEventsRv;
+    @BindView(R.id.addEventFab)
+    FloatingActionButton mAddEventBtn;
+    @BindView(R.id.configureCountTv)
+    TextView mConfigureCompteTv;
     private EventAdapter mEventAdapter;
+    private long mId;
 
 
     @Nullable
@@ -54,6 +72,9 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
         CalendarService.syncCalendars(Objects.requireNonNull(getActivity()));
 
         if (sharedPref.getBoolean("isEmailConfigured", false)) {
+
+            mId = getCalendarId(getActivity(), sharedPref.getString("email", null));
+
             mConfigureCompteTv.setVisibility(View.GONE);
             CalendarService.readCalendar(getContext(), 90, 0);
 
@@ -85,7 +106,6 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
         setCalendarView();
 
 
-
         return view;
     }
 
@@ -103,7 +123,6 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
         Calendar today = Calendar.getInstance(Locale.FRANCE);
         Calendar end = Calendar.getInstance(Locale.FRANCE);
         end.add(Calendar.YEAR, 2);
-        Calendar c = Calendar.getInstance();
 
         eventsCalendar.setToday(today);
         eventsCalendar.setMonthRange(today, end);
@@ -112,7 +131,7 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
         eventsCalendar.setCallback(this);
         eventsCalendar.setSelectionMode(eventsCalendar.getSINGLE_SELECTION());
 
-        if(CalendarService.mEventsList != null && !CalendarService.mEventsList.isEmpty()) {
+        if (CalendarService.mEventsList != null && !CalendarService.mEventsList.isEmpty()) {
             for (CalendarEvent e : CalendarService.mEventsList) {
 
                 String timeStamp = new SimpleDateFormat("YYYY/MM/dd").format(e.getBegin());
@@ -122,18 +141,17 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
     }
 
 
-
     @Override
     public void onDaySelected(@Nullable Calendar selectedDate) {
         String timeStamp = new SimpleDateFormat("YYYY/MM/dd").format(selectedDate.getTime());
-        if(timeStamp.equalsIgnoreCase(new SimpleDateFormat("YYYY/MM/dd").format(Calendar.getInstance().getTime()))
-                && getActivity().getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE).getBoolean("isEmailConfigured", false)){
+        if (timeStamp.equalsIgnoreCase(new SimpleDateFormat("YYYY/MM/dd").format(Calendar.getInstance().getTime()))
+                && getActivity().getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE).getBoolean("isEmailConfigured", false)) {
             mConfigureCompteTv.setVisibility(View.GONE);
             mEventsRv.setVisibility(View.VISIBLE);
             mEventsRv.setAdapter(mEventAdapter);
         } else {
             ArrayList<CalendarEvent> eventsByDate = mEventAdapter.selectDate(timeStamp);
-            if(eventsByDate.isEmpty()){
+            if (eventsByDate.isEmpty()) {
                 mEventsRv.setVisibility(View.GONE);
                 mConfigureCompteTv.setVisibility(View.VISIBLE);
                 mConfigureCompteTv.setText("Aucun événement prévu ce jour la.");
@@ -149,10 +167,11 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
 
     @Override
     public void onDayLongPressed(@Nullable Calendar selectedDate) {
-        TimePickerFragment timePickerFragment = new TimePickerFragment();
+        insertEvent();
+        /*TimePickerFragment timePickerFragment = new TimePickerFragment();
         timePickerFragment.setTargetFragment(AgendaFragment.this, TIME_PICKER);
-        timePickerFragment.show(getFragmentManager(), "timePicker");
-        }
+        timePickerFragment.show(getFragmentManager(), "timePicker");*/
+    }
 
     @Override
     public void onMonthChanged(@Nullable Calendar monthStartDate) {
@@ -161,7 +180,7 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == TIME_PICKER && resultCode == RESULT_OK){
+        if (requestCode == TIME_PICKER && resultCode == RESULT_OK) {
             Bundle bund = data.getExtras();
             int hour = bund.getInt("selectedHour");
             int minute = bund.getInt("selectedMinutes");
@@ -180,5 +199,95 @@ public class AgendaFragment extends Fragment implements EventsCalendar.Callback{
         c.set(Calendar.MILLISECOND, 0);
 
         return (c.getTimeInMillis() / 1000L);
+    }
+
+    private long getCalendarId(Activity activity, String MY_ACCOUNT_NAME) {
+        String[] projection = new String[]{BaseColumns._ID};
+        String selection = CalendarContract.Calendars.ACCOUNT_NAME + " = ?";
+        String[] selArgs = new String[]{MY_ACCOUNT_NAME};
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            return -1;
+        }
+        Cursor cursor = activity.getContentResolver().query(
+                CalendarContract.Calendars.CONTENT_URI, projection, selection, selArgs, null);
+        if (cursor.moveToFirst()) {
+            return cursor.getLong(0);
+        }
+        return -1;
+    }
+
+    private long getGoogleCalendarId() {
+
+        String[] projection = new String[]{CalendarContract.Calendars._ID, CalendarContract.Calendars.NAME, CalendarContract.Calendars.ACCOUNT_NAME, CalendarContract.Calendars.ACCOUNT_TYPE,
+
+        };
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "-1", Toast.LENGTH_SHORT).show();
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return -1;
+        }
+        Cursor calCursor = Objects.requireNonNull(getContext()).getContentResolver()
+                .query(CalendarContract.Calendars.CONTENT_URI, projection, CalendarContract.Calendars.VISIBLE + " = 1", null, CalendarContract.Calendars._ID + " ASC");
+
+        assert calCursor != null;
+        if (calCursor.moveToFirst()) {
+            do {
+                long id = calCursor.getLong(0);
+                String displayName = calCursor.getString(1);
+
+                if (displayName.contains("@gmail.com")) {
+                    return id;
+                }
+
+            } while (calCursor.moveToNext());
+        }
+        calCursor.close();
+        return -1;
+
+    }
+
+    public void insertEvent() {
+        long k = getGoogleCalendarId();
+        long startMillis;
+        long endMillis;
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2018, 12, 16, 7, 30);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2018, 12, 16, 8, 45);
+        endMillis = endTime.getTimeInMillis();
+
+        ContentResolver cr = Objects.requireNonNull(getContext()).getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(CalendarContract.Events.DTSTART, startMillis);
+        values.put(CalendarContract.Events.DTEND, endMillis);
+        values.put(CalendarContract.Events.TITLE, "Jazzercise");
+        values.put(CalendarContract.Events.DESCRIPTION, "Group workout");
+        values.put(CalendarContract.Events.CALENDAR_ID, getGoogleCalendarId());
+        values.put(CalendarContract.Events.ORGANIZER, "zaffransamuel@gmail.com");
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "Europe/Berlin");
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "Missing Permission", Toast.LENGTH_SHORT).show();
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+        assert uri != null;
+        long eventId = Long.valueOf(uri.getLastPathSegment());
+        Toast.makeText(getContext(), eventId + "", Toast.LENGTH_SHORT).show();
+        Log.i("adding event to Cal: ", eventId + "");
     }
 }
